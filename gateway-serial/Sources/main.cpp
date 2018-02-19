c4f225c6c152c801328cafb575bf43a6e31ed1e5
Andrei Tatar <tatarandrei@gmail.com>
2018-02-19 21:19:06 +0200

    integrate changes in gateway

--- gateway-serial/Sources/main.cpp
+++ gateway-serial/Sources/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "radio/RFM69.h"
 
-RFM69 radio(spi_Transfer, true);
+RFM69 radio(spi_Transfer, millis, true);
 bool inited = false;
 
 void setup() {
@@ -13,34 +13,26 @@ void setup() {
 }
 
 void loop() {
-	/*if (radio.receiveDone()) {
-		char data[100];
-		int size = sprintf(data, "F: %d, L: %d, R: %d, %s\r\n", radio.SENDERID, radio.DATALEN, radio.RSSI, radio.DATA);
-		sendBlock((uint8_t*)data, size);
-	}*/
-	
 	static uint32_t next;
 	if (millis() > next) {
-		next = millis() + 1000;
-		
-		uint32_t start = millis();
-		radio.send(2, (uint8_t*)"Hi", 2);
-		uint32_t time = millis() - start;
-		char data[100];
-		int size = sprintf(data, "Time: %d\r\n", time);
-		sendBlock((uint8_t*)data, size);
+		next = millis() + 5000;
+		radio.send(2, (uint8_t*)"Hi2", 3);
 	}
-	
 }
 
 PE_ISR(portDInterrupt) {
 	static RfmPacket packet;
 	if (PORT_PDD_GetPinInterruptFlag(PORTD_BASE_PTR, 4)) {
 		PORT_PDD_ClearPinInterruptFlag(PORTD_BASE_PTR, 4);
+		if (!inited) return;
 		packet.size = 0;
 		radio.interrupt(packet);
+		
 		if (packet.size) {
-			
+			char data[100];
+			packet.data[packet.size] = 0;
+			int size = sprintf(data, "F: %d, L: %d, R: %d, %s\r\n", packet.from, packet.size, packet.rssi, packet.data);
+			sendBlock((uint8_t*)data, size);
 		}
 	}
 }
