31f92ea29f36654f5506deb0da9d141110bac34a
Andrei Tatar <tatarandrei@gmail.com>
2018-03-31 16:41:10 +0300

    progress on amp module

--- sensor-amp/src/main.cpp
+++ sensor-amp/src/main.cpp
@@ -5,6 +5,11 @@
 #define PIN_REMOTE 12
 #define PIN_VOL A0
 
+#define IR_MUTE 0x5ea138c7
+#define IR_VOL_UP 0x5ea158a7
+#define IR_VOL_DOWN 0x5ea1d827
+#define IR_POWER 0x7e8154ab
+
 void setup()
 {
     for (uint8_t ch = 0; ch < 8; ch++)
@@ -15,7 +20,10 @@ void setup()
     pinMode(PIN_ALLOW, OUTPUT);
     digitalWrite(PIN_ALLOW, HIGH);
     pinMode(PIN_REMOTE, OUTPUT);
-    digitalWrite(PIN_REMOTE, LOW);
+    digitalWrite(PIN_REMOTE, HIGH);
+
+    delay(2000); //ignore ESP startup
+
     Serial.begin(57600);
 }
 
@@ -43,11 +51,22 @@ void space(uint16_t time)
     delayMicroseconds(time);
 }
 
-void sendCode(uint32_t code)
+void suspendIr()
 {
     digitalWrite(PIN_ALLOW, LOW);
+    digitalWrite(PIN_REMOTE, LOW);
     delay(10);
+}
 
+void resumeIr()
+{
+    delay(10);
+    digitalWrite(PIN_ALLOW, HIGH);
+    digitalWrite(PIN_REMOTE, HIGH);
+}
+
+void sendRaw(uint32_t code)
+{
     mark(9000);
     space(4500);
 
@@ -62,9 +81,52 @@ void sendCode(uint32_t code)
 
     mark(562);
     space(0);
+}
 
-    delay(5);
-    digitalWrite(PIN_ALLOW, HIGH);
+void sendRepeat()
+{
+    mark(9000);
+    space(2250);
+    mark(562);
+    space(0);
+}
+
+void sendCode(uint32_t code)
+{
+    suspendIr();
+    sendRaw(code);
+    resumeIr();
+}
+
+uint8_t getVolume()
+{
+    return analogRead(PIN_VOL) >> 2;
+}
+
+void setVolume(uint8_t vol)
+{
+    suspendIr();
+    uint8_t current = getVolume();
+    int16_t delta = (int16_t)vol - current;
+    if (abs(delta) > 2)
+    {
+        uint32_t nextSend = millis() + 50;
+        sendRaw(delta > 0
+                    ? IR_VOL_UP
+                    : IR_VOL_DOWN);
+        while (abs((int16_t)vol - current) > 2)
+        {
+            current = getVolume();
+            uint32_t now = millis();
+            if (now >= nextSend)
+            {
+                nextSend = now + 108;
+                sendRepeat();
+            }
+        }
+    }
+    delay(25);
+    resumeIr();
 }
 
 void loop()
@@ -75,12 +137,25 @@ void loop()
         Serial.print("power: ");
         Serial.println(digitalRead(PIN_POWER), 10);
         Serial.print("vol: ");
-        Serial.println(analogRead(PIN_VOL), 10);
+        Serial.println(getVolume(), 10);
         Serial.print("ch: ");
         Serial.println(getActiveChannel(), 10);
         break;
     case 'p':
-        sendCode(0x7e8154ab);
+        sendCode(IR_POWER);
+        break;
+    case 'M':
+        setVolume(50);
+        break;
+    case 'm':
+        setVolume(10);
+        Serial.println("ok;");
+        break;
+    case 's':
+        digitalWrite(PIN_ALLOW, LOW);
+        break;
+    case 'r':
+        digitalWrite(PIN_ALLOW, HIGH);
         break;
     }
 }
