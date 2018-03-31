979cfacb2a58b355d52650040232b32632e76543
Andrei Tatar <tatarandrei@gmail.com>
2018-03-31 21:31:19 +0300

    fix config and path

--- gateway-node/src/util.ts
+++ gateway-node/src/util.ts
@@ -8,7 +8,7 @@ export function getBaseLayer(address: string, logger: Logger) {
     switch (url.protocol) {
         case 'serial:':
             const baudRate = parseInt(url.searchParams.get('baudRate'), 10) || undefined;
-            return new SerialLayer(logger, url.host, baudRate);
+            return new SerialLayer(logger, url.pathname, baudRate);
         case 'telnet:':
             const port = parseInt(url.port, 10) || undefined;
             return new Telnet(logger, url.hostname, port);
