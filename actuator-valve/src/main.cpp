a10a33928bbcc36b541cb0ed095527bae5244ff1
Andrei Tatar <tatarandrei@gmail.com>
2018-12-10 09:20:26 +0200

    progress on integrating Sensor lib

--- actuator-valve/src/main.cpp
+++ actuator-valve/src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
+#include "sensor.h"
 #include "hal/lcd.h"
 #include "hal/motor.h"
 #include "hal/input.h"
 #include "hal/sleep.h"
 #include "hal/zerolcd.h"
-
 #include "states/state.h"
+
+void spiTransfer(uint8_t *data, uint8_t len)
+{
+    while (len--)
+    {
+        SPDR = *data;
+        while (!(SPSR & (1 << SPIF)))
+            ;
+        *data++ = SPDR;
+    }
+}
+
+Sensor sensor(spiTransfer);
 
 void setup()
 {
+    DDRB = (1 << PB2) | (1 << PB1);
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPI2X);
+
     motorInit();
     inputInit();
     initLCD();
@@ -16,6 +32,8 @@ void setup()
 
     Lcd_Symbol(RADIO, 1);
     Lcd_Symbol(AUTO, 1);
+
+    
 }
 
 void loop()
