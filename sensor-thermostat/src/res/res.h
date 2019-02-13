a1ead4ae8bde4623b59b6ca6f7abb9ca8ea1d124
Andrei Tatar <tatarandrei@gmail.com>
2019-02-13 22:55:55 +0200

    progress on interface

---
+++ sensor-thermostat/src/res/res.h
@@ -0,0 +1,30 @@
+#ifndef _RES_H_
+#define _RES_H_
+
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+typedef struct
+{
+    const uint8_t *data;
+    uint16_t width;
+    uint16_t height;
+} tImage;
+
+typedef struct
+{
+    long int code;
+    const tImage *image;
+} tChar;
+
+typedef struct
+{
+    int length;
+    const tChar *chars;
+} tFont;
+
+extern const tImage img_error;
+extern const tFont roboto_big;
+extern const tFont roboto_medium;
+
+#endif
--- sensor-thermostat/src/ui.cpp
+++ sensor-thermostat/src/ui.cpp
@@ -3,10 +3,49 @@
 #include <U8g2lib.h>
 #include <touch.h>
 #include <rtc.h>
+#include "res/res.h"
 
 #define OLED_POWER A3
 
 static U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0);
+
+static void drawImage(uint8_t x, uint8_t y, const tImage &img)
+{
+    u8g2.drawXBMP(x, y, img.width, img.height, img.data);
+}
+
+static uint8_t drawChar(uint8_t x, uint8_t y, uint8_t ch, const tFont &font)
+{
+    for (uint8_t i = 0; i < font.length; i++)
+    {
+        const tChar &fontChar = font.chars[i];
+        if (fontChar.code == ch)
+        {
+            const tImage &img = *fontChar.image;
+            drawImage(x, y, img);
+            return img.width;
+        }
+    }
+    return 0;
+}
+
+static void drawValue(uint8_t x, uint8_t y, uint16_t value)
+{
+    uint8_t v = value / 1000 % 10;
+    if (v)
+    {
+        x += drawChar(x, y, '0' + v, roboto_big);
+    }
+    v = value / 100 % 10;
+    if (v)
+    {
+        x += drawChar(x, y, '0' + v, roboto_big);
+    }
+    v = value / 10 % 10;
+    x += drawChar(x, y, '0' + v, roboto_big);
+    x += drawChar(x, y, '.', roboto_medium);
+    drawChar(x, y, '0' + value % 10, roboto_medium);
+}
 
 void uiInit()
 {
@@ -26,15 +65,22 @@ bool uiLoop()
     if (key != lastKey)
     {
         lastKey = key;
-        char str[2];
-        str[0] = key % 10 + '0';
-        str[1] = 0;
+
+        static uint16_t value = 235;
+        switch (key)
+        {
+        case KEY_DOWN:
+            value -= 1;
+            break;
+        case KEY_UP:
+            value += 1;
+            break;
+        }
 
         u8g2.firstPage();
         do
         {
-            u8g2.setFont(u8g2_font_ncenB14_tr);
-            u8g2.drawStr(0, 24, str);
+            drawValue(10, (64 - roboto_big.chars[0].image->height) / 2, value);
         } while (u8g2.nextPage());
         nextSleep = rtcTime() + 5;
     }
